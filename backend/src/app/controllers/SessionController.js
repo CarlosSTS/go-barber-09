ceafc191c5ed8a7b24708905bd4271411c0692b6
Francisco Carlos Ferreira <franciscocarlosferreira@Franciscos-MacBook-Pro.local>
2021-03-19 15:24:47 -0300

    application started

---
+++ backend/src/app/controllers/SessionController.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import authConfig from '../../config/auth'
+
+class SessionsController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    try {
+      await schema.validate(request.body);
+    } catch (err) {
+      return response.status(422).json({ error: `Validation fails: ${err.message}` });
+    }
+
+    const { email, password } = request.body
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return response.status(401).json({ error: 'User not found' })
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return response.status(401).json({ error: 'Password does not match' })
+    }
+
+    const { id, name } = user
+
+    return response.json({
+      user: {
+        id, name, email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+export default new SessionsController();
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3a8268e716aa23db66026e7843b65edd',
+  expiresIn: '1d'
+};
